85ab958b939e1267f7968669b929a0d65be9af43
Dev U. <matrix.h225@gmail.com>
2025-10-22 21:22:24 +0530

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import numpy as np
 from rdkit import RDLogger
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score, mean_absolute_percentage_error
